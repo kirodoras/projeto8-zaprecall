f4d26861e138c162c307257a156b8efdbac2af6a
MateusFigueiredo <figueiredo1497@gmail.com>
2022-05-14 11:01:24 -0300

    Componetizing <card>, animation cards

--- src/components/playscreen/CardsPlace.js
+++ src/components/playscreen/CardsPlace.js
@@ -1,35 +1,88 @@
+import React from 'react';
+
+const deckReact = [
+    {
+        question: 'O que é JSX?', answer: 'Uma extensão de linguagem do JavaScript'
+    },
+    {
+        question: 'O React é __ ', answer: 'uma biblioteca JavaScript para construção de interfaces'
+    },
+    {
+        question: 'Componentes devem iniciar com __', answer: 'letra maiúscula'
+    },
+    {
+        question: 'Podemos colocar __ dentro do JSX', answer: 'expressões'
+    },
+    {
+        question: 'O ReactDOM nos ajuda __ ', answer: 'interagindo com a DOM para colocar componentes React na mesma'
+    },
+    {
+        question: 'Usamos o npm para __', answer: 'gerenciar os pacotes necessários e suas dependências'
+    },
+    {
+        question: 'Usamos props para __', answer: 'passar diferentes informações para componentes '
+    },
+    {
+        question: 'Usamos estado (state) para __', answer: 'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente'
+    },
+];
+
 export default function CardsPlace() {
     return (
         <div className="cardsPlace">
-            <div className="cardAnswer rotate">
-                <div className="frontCardAnswer">
-                    <span>O que é JSX?</span>
-                    <ion-icon name="reload-outline"></ion-icon>
+            {deckReact.map((value, index) => <Card
+                question={value.question}
+                answer={value.answer}
+                cardNumber={index + 1}
+                key={index} />)}
+        </div>
+    );
+}
+
+function Card(props) {
+    //LOGIC
+    const [seeQuestion, setSeeQuestion] = React.useState(false);
+    //UI
+    return (
+        <>
+            {
+                seeQuestion ? <CardHidden question={props.question} answer={props.answer}/> :
+                    <CardShow cardNumber={props.cardNumber} changeCard={() => setSeeQuestion(true)} />
+            }
+        </>
+    );
+
+}
+
+
+function CardShow({ cardNumber, changeCard, color }) {
+    //console.log(props)
+    return (
+        <div className="card" onClick={changeCard}>
+            <h2 className={color}>Pergunta {cardNumber}</h2>
+            <ion-icon class={color} name="play-outline"></ion-icon>
+        </div>
+    );
+}
+
+function CardHidden({question, answer}) {
+    //LOGIC
+    const [rotate, setRotate] = React.useState('');
+
+    //UI
+    return (
+        <div className={`cardAnswer ${rotate}`}>
+            <div className="frontCardAnswer">
+                <span>{question}</span>
+                <ion-icon name="reload-outline" onClick={() => setRotate('rotate')}></ion-icon>
+            </div>
+            <div className="backCardAnswer">
+                <span>{answer}</span>
+                <div className="answerButtons">
+                    <button type="button">Não<br />lembrei</button>
+                    <button type="button">Quase não<br />lembrei</button>
+                    <button type="button">Zap!</button>
                 </div>
-                <div className="backCardAnswer">
-                    <span>JSX é uma sintaxe para escrever HTML dentro do JS</span>
-                    <div className="answerButtons">
-                        <button type="button">Não<br />lembrei</button>
-                        <button type="button">Quase não<br />lembrei</button>
-                        <button type="button">Zap!</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card play-red">
-                <h2>Pergunta 1</h2>
-                <ion-icon name="play-outline"></ion-icon>
-            </div>
-            <div className="card">
-                <h2>Pergunta 2</h2>
-                <ion-icon name="play-outline"></ion-icon>
-            </div>
-            <div className="card">
-                <h2>Pergunta 3</h2>
-                <ion-icon name="play-outline"></ion-icon>
-            </div>
-            <div className="card">
-                <h2>Pergunta 4</h2>
-                <ion-icon name="play-outline"></ion-icon>
             </div>
         </div>
     );
