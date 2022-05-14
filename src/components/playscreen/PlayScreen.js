729c8a26d183227c9b128bbf78268b296f5eb272
MateusFigueiredo <figueiredo1497@gmail.com>
2022-05-13 22:04:00 -0300

    Componetizing <playfooter>

--- src/components/playscreen/PlayScreen.js
+++ src/components/playscreen/PlayScreen.js
@@ -1,4 +1,5 @@
 import CardsPlace from "./CardsPlace";
+import PlayFooter from "./PlayFooter";
 
 export default function PlayScreen() {
     return (
@@ -10,32 +11,7 @@ export default function PlayScreen() {
                 </div>
                 <CardsPlace />
             </div>
-            <div className="playFooter">
-                <div className="message hidden">
-                    <div className="messageHeader">
-                        <img src="assets/party.png" alt="Party emote" />
-                        <span>Parabéns!</span>
-                    </div>
-                    <div className="messageContent">
-                        Você não esqueceu de nenhum flashcard!
-                    </div>
-                </div>
-                <div className="message">
-                    <div className="messageHeader">
-                        <img src="assets/sad.png" alt="Party emote" />
-                        <span>Putz...</span>
-                    </div>
-                    <div className="messageContent">
-                        Ainda faltam alguns... Mas não desanime!
-                    </div>
-                </div>
-                <span className="playCounter">0/4 CONCLUÍDOS</span>
-                <span>
-                    <ion-icon class="play-orange" name="help-circle"></ion-icon>
-                    <ion-icon class="play-red" name="close-circle"></ion-icon>
-                    <ion-icon class="play-green" name="checkmark-circle"></ion-icon>
-                </span>
-            </div>
+            <PlayFooter />
         </>
     );
 }
